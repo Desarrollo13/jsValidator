1d97ab93abfffe3f87e9f0450d812bbf2d2eea59
cristian <cristianmesa13@gmail.com>
2021-04-29 14:01:35 -0300

    validacion numerica y alfanumerica completa

--- js/jsValidator.js
+++ js/jsValidator.js
@@ -2,8 +2,17 @@ class JsValidator {
   status = true;
   errors = [];
   via = "http";
+  validators = {
+    minLength: 3,
+    maxLength: 255,
+  };
   msg = {
     required: `Este campo es requirido`,
+    minLength: `Longitud no valida. Minimo __minLength__ caracteres.`,
+    maxLength: `Longitud no valida. Maximo __maxLength__ caracteres.`,
+    email: `El campo de email no es valido`,
+    integer: `Por favor coloca un numero entero`,
+    alphanumeric: `Solo se permite letras y numeros sin espacios`,
   };
 
   constructor(formid) {
@@ -155,5 +164,51 @@ JsValidator.prototype._required = function (input) {
   }
 };
 JsValidator.prototype._length = function (input) {
-  // aca va toda la logica de validacion
+  let value = input.value;
+  let inputLength = value.length;
+  let minLength =
+    input.dataset.validators_minlength !== undefined
+      ? Number(input.dataset.validators_minlength)
+      : this.validators.minLength;
+  let maxLength =
+    input.dataset.validators_maxlength !== undefined
+      ? Number(input.dataset.validators_maxlength)
+      : this.validators.maxLength;
+  let msg;
+
+  if (inputLength < minLength) {
+    msg = this.msg.minLength.replace("__minLength__", minLength);
+    this.setError(input, msg);
+  }
+  if (inputLength > maxLength) {
+    msg = this.msg.maxLength.replace("__maxLength__", maxLength);
+    this.setError(input, msg);
+  }
+};
+JsValidator.prototype._email = function (input) {
+  let value = input.value;
+  let msg = this.msg.email;
+  let pattern = new RegExp(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i
+  );
+  if (!pattern.test(value) && value.trim() != "") {
+    this.setError(input, msg);
+  }
+};
+JsValidator.prototype._integer = function (input) {
+  let value = input.value;
+  let msg = this.msg.integer;
+
+  let pattern = new RegExp(/^[0-9]+$/);
+  if (!pattern.test(value) && value.trim() != "") {
+    this.setError(input, msg);
+  }
+};
+JsValidator.prototype._alphanumeric = function (input) {
+  let value = input.value;
+  let msg = this.alphanumeric;
+  let pattern = new RegExp(/^[a-zA-Z0-9]+$/);
+  if (!pattern.test(value) && value.trim() != "") {
+    this.setError(input, msg);
+  }
 };
